a64144fec9a352340deed2abcd5b5dff45bf3999
Sinolinaa <sinolinaasherline@gmail.com>
2024-08-14 03:31:03 +0000

    m

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,13 @@
 docker compose up -d
 
+if [ "$BRANCH_NAME" == "dev" ]; then
+
 docker tag  mynewreactapp sinolinaa/dev:v1.0
 
 docker push sinolinaa/dev:v1.0
+
+else
+  echo "Branch is not dev. Skipping Docker tag and push."
+fi
 
 
