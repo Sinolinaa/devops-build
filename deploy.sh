7dd38b746b446a2059a0edd65624bac1ac5d1d04
Sinolinaa <sinolinaasherline@gmail.com>
2024-08-14 13:48:41 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,12 @@ current_branch=$(git rev-parse --abbrev-ref HEAD)
 
 # Print the current branch name
 echo "Current branch is: $current_branch"
+
 if [[ $GIT_BRANCH == "origin/dev" ]]; then
-
 docker tag  mynewreactapp sinolinaa/dev
-
 docker push sinolinaa/dev
 
 elif [[ $GIT_BRANCH == "origin/master" ]]; then
 docker tag  mynewreactapp sinolinaa/prod
-
 docker push sinolinaa/prod
 fi
